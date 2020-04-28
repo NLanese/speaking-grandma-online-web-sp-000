96fa8992872bce1b73339fc2912ccd596d38d8ab
Nick Lanese <nlanese21@gmail.com>
2020-04-28 01:40:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   loudPhrase = phrase.upcase
-  if (phrase == "I LOVE YOU GRANDMA")
+  if (phrase == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   elsif (phrase == loudPhrase)
       return "NO, NOT SINCE 1938!"
